5257f810b204415ce3b16a291f3d1cc21fc5e801
Eric <zdou@student.unimelb.edu.au>
2018-04-25 19:27:25 +1000

    Modified Control

--- src/activitystreamer/server/Connection.java
+++ src/activitystreamer/server/Connection.java
@@ -24,8 +24,7 @@ public class Connection extends Thread {
 	private boolean open = false;
 	private Socket socket;
 	private boolean term=false;
-	
-//	private 
+	 
 	
 	Connection(Socket socket) throws IOException{
 		in = new DataInputStream(socket.getInputStream());
--- src/activitystreamer/server/Control.java
+++ src/activitystreamer/server/Control.java
@@ -15,9 +15,16 @@ import org.json.simple.JSONObject;
 
 public class Control extends Thread {
 	private static final Logger log = LogManager.getLogger();
-
+	
+	private static ArrayList<Connection> connections;
+	//Client connection
 	private static ArrayList<Connection> loadConnections;
+	//Server connection
 	private static ArrayList<Connection> broadConnections;
+	
+	
+	//Database that keeps track of number of clients connected to each server
+	private static HashMap<String,Integer> clientNum = new HashMap<>();
 
 	private static boolean term=false;
 	private static Listener listener;
@@ -28,19 +35,26 @@ public class Control extends Thread {
 	private final static String REGISTER = "REGISTER";
 	private final static String REGISTER_SUCCESS = "REGISTER_SUCCESS";
 	private final static String REGISTER_FAILED = "REGISTER_FAILED";
-	private final static String ANONYMOUS_USERNAME = "ANONYMOUS_USERNAME";
+	private final static String ANONYMOUS_USERNAME = "anonymous";
 
 	private final static String LOGIN = "LOGIN";
 	private final static String LOGIN_SUCCESS = "LOGIN_SUCCESS";
 	private final static String LOGIN_FAILED = "LOGIN_FAILED";
 	private final static String LOGOUT = "LOGOUT";
+	
+	private final static String LOCK_REQUEST = "LOCK_REQUEST";
 
+	//
 	private final static String CLIENT_AUTHENTICATE = "CLIENT_AUTHENTICATE";
+	
+	
 	private final static String ACTIVITY_BROADCAST = "ACTIVITY_BROADCAST";
 	private final static String ACTIVITY_MESSAGE = "ACTIVITY_MESSAGE";
 
 	private final static String AUTHENTICATION_FAIL = "AUTHTENTICATION_FAIL";
 	private final static String AUTHENTICATE = "AUTHENTICATE";
+	
+	//
 	private final static String REPEATED_AUTHENTICATION = "REPEATED_AUTHENTICATION";
 	private final static String UNAUTHENTICATED_SERVER = "UNAUTHENTICATED_SERVER";
 
@@ -49,7 +63,6 @@ public class Control extends Thread {
 	
 	protected static Control control = null;
 	
-	//static 这个类的类对象/类方法，通过类名调用，包含了这个类的方法。如果不是类方法，需要类对象调用，listener要调用的话得通过静态方法。这样不管怎么调用都是同一个control就是
 	public static Control getInstance() {
 		if(control==null){
 			control=new Control();
@@ -95,7 +108,7 @@ public class Control extends Thread {
 	 * logout
 	 * active
 	 */
-	public synchronized boolean process(Connection con,String msg){
+	public synchronized boolean process(Connection con, String msg){
 		JsonObject receivedMSG;
 		String command;
 		String info;
@@ -117,19 +130,24 @@ public class Control extends Thread {
 		String message = receivedMSG.get("command").getAsString();
 		switch(message){
 			case REGISTER:
-				return register(con, receivedMSG);
+				if(!loadConnections.contains(con)) {
+					loadConnections.add(con);
+				}
+				//return register(con, receivedMSG);
 			case LOGIN:
-				return login(con, receivedMSG);
+				return !login(con, receivedMSG);
 			case AUTHENTICATE:
-				return auth(con, receivedMSG);
+				return !auth(con, receivedMSG);
 			case INVALID_MESSAGE:
-				return invalid(con, receivedMSG);
+				//return invalid(con, receivedMSG);
 			case SERVER_ANNOUNCE:
-				return announce(con, receivedMSG);
+				//return announce(con, receivedMSG);
 			case LOGOUT:
-				return logout(con, receivedMSG);
+				//return logout(con, receivedMSG);
+			case LOCK_REQUEST:
+				//return lockRequest(con,receivedMSG);
 			default:
-				return processInvalidCommand(con, receivedMSG);
+				//return processInvalidCommand(con, receivedMSG);
 		}
 	}
 	
@@ -138,6 +156,8 @@ public class Control extends Thread {
 	 */
 	public synchronized void connectionClosed(Connection con){
 		if(!term) connections.remove(con);
+		if(loadConnections.contains(con)) loadConnections.remove(con);
+		if(broadConnections.contains(con)) broadConnections.remove(con);
 	}
 	
 	/*
@@ -157,43 +177,48 @@ public class Control extends Thread {
 	public synchronized Connection outgoingConnection(Socket s) throws IOException{
 		log.debug("outgoing connection: "+Settings.socketAddress(s));
 		Connection c = new Connection(s);
-		loadConnections.add(c);
+		connections.add(c);
 		return c;
 		
 	}
 
-	private boolean validUserInfo(Connection con, JsonObject receivedMSG) {
-		InvalidMessage invalidMsg = new InvalidMessage();
-		if (!receivedMSG.has("username")) {
-			invalidMsg.setInfo("username must be contained");
-			con.writeMsg(invalidMsg.toJsonString());
-			return false;
-		} else if (!receivedMSG.has("secret")) {
-			invalidMsg.setInfo("secret must be contained");
-			con.writeMsg(invalidMsg.toJsonString());
-			return false;
-		} else {
-			return true;
-		}
-	}
+	
+//	//This is for checking if the message contain valid username and secret
+//	private boolean validUserInfo(Connection con, JsonObject receivedMSG) {
+//		InvalidMessage invalidMsg = new InvalidMessage();
+//		if (!receivedMSG.has("username")) {
+//			invalidMsg.setInfo("username must be contained");
+//			con.writeMsg(invalidMsg.toJsonString());
+//			return false;
+//		} else if (!receivedMSG.has("secret")) {
+//			invalidMsg.setInfo("secret must be contained");
+//			con.writeMsg(invalidMsg.toJsonString());
+//			return false;
+//		} else {
+//			return true;
+//		}
+//	}
 
-	//封装JSON有点问题。。不知道command和info怎么弄到一起去然后现在这个JSONObject大概要重写一个write= =
+	
+	@SuppressWarnings("unchecked")
 	private boolean register(Connection con, JsonObject receivedMSG){
 		String command;
 		String info;
 		JSONObject regist = new JSONObject();
 		if(!validUserInfo(con, receivedMSG)){
-			return true;
+			return false;
 		}
 		String secret = receivedMSG.get("secret").getAsString();
 		String username = receivedMSG.get("username").getAsString();
+		
+		//Still need to send lock request etc.
 		if (userInfo.containsKey(username) || username.equals(ANONYMOUS_USERNAME)){
 			command = REGISTER_FAILED;
 			//String registFailedMSG = toJsonString(command);
 			info = username + " is already registered with the system.";
 			//String registFailedInfo = toJsonString(info);
 			regist.put(command,info);
-			con.writeMsg(regist);
+			con.writeMsg(regist.toJSONString());
 			return true;
 		}else{
 			command = REGISTER_SUCCESS;
@@ -201,79 +226,161 @@ public class Control extends Thread {
 			info = "register success for " + username;
 			//String registSuccessInfo = toJsonString(info);
 			regist.put(command,info);
-			con.writeMsg(regist);
+			con.writeMsg(regist.toJSONString());
 			userInfo.put(username,secret);
 			return true;
 		}
 	}
 
+	
+	/**Process the LOGIN command from client
+	 * 
+	 * @param con The connection from which the message is sent
+	 * @param receivedMSG The coming message
+	 * @return True if login successful, false otherwise
+	 */
+//	@SuppressWarnings("unchecked")
+//	private boolean login(Connection con, JsonObject receivedMSG){
+//		JSONObject login = new JSONObject();
+//		String command;
+//		String info;
+//		if(!validUserInfo(con, receivedMSG)){
+//			return false;
+//		}
+//		String secret = receivedMSG.get("secret").getAsString();
+//		String username = receivedMSG.get("username").getAsString();
+//		//这里应该是登录成功然后redirect，缺一段找其他server的代码
+//		if( (username.equals(ANONYMOUS_USERNAME) && secret == null) ||
+//				userInfo.get(username).equals(secret)){
+//			command = LOGIN_SUCCESS;
+//			//String loginSuccessMSG = toJsonString(command);
+//			info = "logged in as user " + username;
+//			//String loginSucessInfo = toJsonString()
+//			login.put("command",command);
+//			login.put("info", info);
+//			con.writeMsg(login.toJSONString());
+//			
+//			//Check and move the connection to client connection list
+//			if(!loadConnections.contains(con))
+//				loadConnections.add(con);
+//			
+//			//Lock request
+//			
+//			
+//	}
+//		else{
+//			command = LOGIN_FAILED;
+//			//String loginFailedMSG = toJsonString(command);
+//			info = "attempt to login with wrong secret";
+//			//String loginFailedInfo = toJsonString(info);
+//			login.put(command,info);
+//			con.writeMsg(login.toJSONString());
+//			return true;
+//		}
+//		
+//		return true;
+//	}
 	private boolean login(Connection con, JsonObject receivedMSG){
 		JSONObject login = new JSONObject();
 		String command;
 		String info;
-		if(!validUserInfo(con, receivedMSG)){
-			return true;
-		}
-		String secret = receivedMSG.get("secret").getAsString();
-		String username = receivedMSG.get("username").getAsString();
-		if(!username.equals(ANONYMOUS_USERNAME) && !(userInfo.containsKey(username)
-				&& userInfo.get(username).equals(secret))){
-			command = LOGIN_FAILED;
-			//String loginFailedMSG = toJsonString(command);
-			info = "attempt to login with wrong secret";
-			//String loginFailedInfo = toJsonString(info);
-			login.put(command,info);
-			con.writeMsg(login);
-			return true;
-		}
-		//这里应该是登录成功然后redirect，缺一段找其他server的代码
+		int num;
+		if(validUserInfo(con, receivedMSG)){		
+			String secret = receivedMSG.get("secret").getAsString();
+			String username = receivedMSG.get("username").getAsString();
+			if(!username.equals(ANONYMOUS_USERNAME){
+				if(userInfo.containsKey(username)){
+					if(userInfo.get(username).equals(secret))){
+			        	num=clientNum.get(Settings.getRemoteHostname());
+			        	for(String getKey:clientNum.keySet()){  
+			        		if(clientNum.get(getKey)+2<num){  
+			        			key = getKey; 
+			        			login.put("command","REDERICT");
+			        			login.put("hostname",空白);
+			        			login.put("port",空白);
+			        			return false;
+			        		}
+			        	}
+			        	command = LOGIN_SUCCESS;
+		        		info = "logged in as user " + username;
+		        		login.put("command",command);
+		        		login.put("info",info);
+		        		return true;
+		        }
+			 else {
+		        	command=LOGIN_FAILED;
+		        	info = "attempt to login with wrong secret";
+		        	login.put("command",command);
+					login.put("info",info);
+					return false;
+			}			
+		}	
+				else {
+					command=LOGIN_FAILED;
+		        	info = "attempt to login with wrong username";
+		        	login.put("command",command);
+					login.put("info",info);
+					return false;
+				}
+			}	
 		else {
 			command = LOGIN_SUCCESS;
-			//String loginSuccessMSG = toJsonString(command);
-			info = "logged in as user " + username;
-			//String loginSucessInfo = toJsonString()
-			login.put(command,info);
-			con.writeMsg(login);
-			//if有空 else都很忙
+			info = "logged in as user " + ANONYMOUS_USERNAME;
+			login.put("command",command);
+			login.put("info",info);
+			return true;
 		}
-		return true;
+	   }
 	}
+		
 
+	
+	/**Process the AUTHENTICATE command from other server
+	 * @param con  The connection from which the message comes
+	 * @param receivedMSG The received message
+	 * @return True if authenticate success, false otherwise
+	 */
+	@SuppressWarnings("unchecked")
 	private boolean auth(Connection con, JsonObject receivedMSG){
 		JSONObject auth = new JSONObject();
 		String command;
 		String info;
+		
 		if(!receivedMSG.has("secret")){
 			command = AUTHENTICATION_FAIL;
 			//String authFailMSG = toJsonString();
 			info = "the supplied secret is incorrect";
-			auth.put(command,info);
-			con.writeMsg(auth);
-			return  true;
+			auth.put("command", command);
+			auth.put("info",info);
+			con.writeMsg(auth.toJSONString());
+			return false;
 		}
+		
 		String secret = receivedMSG.get("secret").getAsString();
 		if(!secret.equals(Settings.getSecret())){
 			command = AUTHENTICATION_FAIL;
 			//String authFailMSG = toJsonString();
 			info = "the supplied secret is incorrect: " + secret;
-			auth.put(command,info);
-			con.writeMsg(auth);
-			return  true;
+			auth.put("command",command);
+			auth.put("info",info);
+			con.writeMsg(auth.toJSONString());
+			return false;
 		}
-		for (Connection connection : loadConnections) {
-			if (con.getSocket().getPort() == connection.getSocket().getPort()) {
+		
+		//Check if the connection is already authenticated
+		for (Connection connection : broadConnections) {
+			if (con.getSocket().getInetAddress() == connection.getSocket().getInetAddress()) {
 				InvalidMessage invalidMsg = new InvalidMessage();
 				invalidMsg.setInfo(REPEATED_AUTHENTICATION);
 				con.writeMsg(invalidMsg.toJsonString());
-				return true;
 			}else{
-				loadConnections.add(con);
-				return false;
+				broadConnections.add(con);
 			}
 		}
-		return false;
+		return true;
 	}
 
+	//
 	private boolean invalid(Connection con, JsonObject receivedMSG){
 		String errorInfo = receivedMSG.get("info").getAsString();
 		if (errorInfo.equals(UNAUTHENTICATED_SERVER) ||
@@ -287,6 +394,9 @@ public class Control extends Thread {
 	private boolean logout(Connection con, JsonObject receivedMSG){
 		String logoutInfo = receivedMSG.get("command").getAsString();
 		if(logoutInfo.equals(LOGOUT)){
+			if(loadConnections.contains(con))
+				loadConnections.remove(con);
+			connections.remove(con);
 			return true;
 		}
 		return false;
@@ -294,16 +404,14 @@ public class Control extends Thread {
 
 
 	//这个地方是broadcast了
-	private boolean announce(Connection con, JsonObject receivedMSG){
-		if(!loadConnections.contains(con)){
-			InvalidMessage invalidMsg = new InvalidMessage();
-			invalidMsg.setInfo(UNAUTHENTICATED_SERVER);
-			con.writeMsg(invalidMsg.toJsonString());
-			return true;
-		}
-
-
-	}
+//	private boolean announce(Connection con, JsonObject receivedMSG){
+//		if(!loadConnections.contains(con)){
+//			InvalidMessage invalidMsg = new InvalidMessage();
+//			invalidMsg.setInfo(UNAUTHENTICATED_SERVER);
+//			con.writeMsg(invalidMsg.toJsonString());
+//			return true;
+//		}
+//	}
 
 	private boolean processInvalidCommand(Connection con, JsonObject receivedMSG){
 		String command = receivedMSG.get("command").getAsString();
@@ -312,10 +420,6 @@ public class Control extends Thread {
 		con.writeMsg(invalidMsg.toJsonString());
 		return true;
 	}
-
-
-
-
 
 	
 	@Override
@@ -343,6 +447,8 @@ public class Control extends Thread {
 		listener.setTerm(true);
 	}
 	
+	
+	//Broadcasting message to other server
 	public boolean doActivity(){
 		return false;
 	}
